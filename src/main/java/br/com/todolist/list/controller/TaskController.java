f8f6be7e238cfc21517318413efc6f528364ed10
Leonardo Matheus <leonardomm.contato@gmail.com>
2023-03-17 18:15:57 -0300

    feat(TaskController): add new endpoint for create new task

--- src/main/java/br/com/todolist/list/controller/TaskController.java
+++ src/main/java/br/com/todolist/list/controller/TaskController.java
@@ -2,9 +2,7 @@ package br.com.todolist.list.controller;
 
 import br.com.todolist.list.domain.model.Task;
 import br.com.todolist.list.domain.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,6 +14,12 @@ public class TaskController {
     @GetMapping
     public List<Task> list() {
         return Arrays.asList(new Task(2L, "task 2", new User()), new Task(1L, "task 1", new User()));
+    }
+
+    @PostMapping
+    public Task addNewTask(@RequestBody Task task) {
+        System.out.println(task.getDescription());
+        return new Task(task.getId(), task.getDescription(), task.getUser());
     }
 
 }
