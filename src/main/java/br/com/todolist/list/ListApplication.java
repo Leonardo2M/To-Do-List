084b85569482bdc94686f58a246c7417834914ad
Leonardo Matheus <leonardomm.contato@gmail.com>
2023-03-18 10:03:39 -0300

    fix(task): add new data attribute and change relation to ManyToOne with user

--- src/main/java/br/com/todolist/list/ListApplication.java
+++ src/main/java/br/com/todolist/list/ListApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ListApplication {
+public class ListApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListApplication.class, args);
 	}
+
 
 }
--- src/main/java/br/com/todolist/list/domain/model/Task.java
+++ src/main/java/br/com/todolist/list/domain/model/Task.java
@@ -3,6 +3,8 @@ package br.com.todolist.list.domain.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "tasks")
 @AllArgsConstructor
@@ -16,8 +18,10 @@ public class Task {
     private Long id;
     private String description;
     private Boolean completed = Boolean.FALSE;
+    private LocalDateTime date;
 
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
 }
