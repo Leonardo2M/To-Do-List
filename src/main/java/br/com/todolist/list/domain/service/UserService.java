d8b3c673bba4d0b0f2109ca85d9834d647f4e408
Leonardo Matheus <leonardomm.contato@gmail.com>
2023-03-20 20:43:51 -0300

    feat(UserService): create a new user service with findAll method

---
+++ src/main/java/br/com/todolist/list/domain/service/UserService.java
@@ -0,0 +1,24 @@
+package br.com.todolist.list.domain.service;
+
+import br.com.todolist.list.domain.model.User;
+import br.com.todolist.list.domain.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    private ResponseEntity<List<User>> findAll() {
+        var user = repository.findAll();
+
+        return ResponseEntity.ok().body(user);
+    }
+}
