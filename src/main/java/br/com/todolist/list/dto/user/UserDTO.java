f1dad2fbc9454c0db7c90becc54f03d832654213
Leonardo Matheus <leonardomm.contato@gmail.com>
2023-03-20 22:10:20 -0300

    feat(DTO): create DTO for to detail new user created

---
+++ src/main/java/br/com/todolist/list/dto/user/UserDTO.java
@@ -0,0 +1,16 @@
+package br.com.todolist.list.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String username;
+    private String password;
+
+}
