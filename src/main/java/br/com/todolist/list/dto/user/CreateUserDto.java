fa5475ec4a05afa3535f3755ca574faa792a96a4
Leonardo Matheus <leonardomm.contato@gmail.com>
2023-03-20 22:01:23 -0300

    feat(DTO): create DTO for user entity for registration

---
+++ src/main/java/br/com/todolist/list/dto/user/CreateUserDto.java
@@ -0,0 +1,15 @@
+package br.com.todolist.list.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserDto {
+
+    private String username;
+    private String password;
+
+}
