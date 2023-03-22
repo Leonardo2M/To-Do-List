c339b568e7a289e5b0d92f27763962643271b0c9
Leonardo Matheus <leonardomm.contato@gmail.com>
2023-03-22 00:53:19 -0300

    refactor(CreateUserDTO): refactored with new entity attribute and validations

--- src/main/java/br/com/todolist/list/dto/user/CreateUserDTO.java
+++ src/main/java/br/com/todolist/list/dto/user/CreateUserDTO.java
@@ -1,5 +1,8 @@
 package br.com.todolist.list.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +12,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CreateUserDTO {
 
+    @NotBlank
+    @Min(3)
     private String username;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    @Min(6)
     private String password;
 
 }
